d0744597fa609c4eb9458de1be9d5238ac1569c4
Abhay prajapati <abhayparja90@gmail.com>
2025-05-09 07:37:46 +0530

    Add ci cd pipline

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import userRouter from "./routes/User.routes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import cluster from "cluster";
 import os from "os";
-import {logger} from "./utils/logger.js";
-
-
+import { logger } from "./utils/logger.js";
 
 if (cluster.isMaster) {
   const numCPUs = os.cpus().length;
@@ -24,33 +22,35 @@ if (cluster.isMaster) {
     cluster.fork();
   });
 } else {
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.json());
-// app.use(morgan("combined"));
-// Middleware to log request count per worker
-app.use((req, res, next) => {
-  logger.info(`Request: ${req.method} ${req.url}`);
-  next();
-});
+  const app = express();
+  const PORT = process.env.PORT || 3000;
+  app.use(express.json());
+  // app.use(morgan("combined"));
+  // Middleware to log request count per worker
+  app.use((req, res, next) => {
+    logger.info(`Request: ${req.method} ${req.url}`);
+    next();
+  });
 
-app.get("/", (req, res) => {
-  res.send(
-    `Worker ${process.pid} - Total Requests: ${app.locals.requestCount}`
-  );
-});
+  app.get("/", (req, res) => {
+    res
+      .status(200)
+      .json({
+        message: `Worker ${process.pid} - Total Requests: ${app.locals.requestCount}`,
+      });
+  });
 
-app.use("/api/v1/users", userRouter);
+  app.use("/api/v1/users", userRouter);
 
-app.use((err, req, res, next) => {
-  logger.error(`Error: ${err.message} - ${req.method} ${req.url}`);
-  res.status(500).json({ error: "Something went wrong!" });
-});
+  app.use((err, req, res, next) => {
+    logger.error(`Error: ${err.message} - ${req.method} ${req.url}`);
+    res.status(500).json({ error: "Something went wrong!" });
+  });
 
-app.use(errorHandler);
+  app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Worker ${process.pid} is running on port ${PORT}`);
-});
-connectDB();
+  app.listen(PORT, () => {
+    console.log(`Worker ${process.pid} is running on port ${PORT}`);
+  });
+  connectDB();
 }
